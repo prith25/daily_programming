7c17db02918b1e04cc6abceb5d3dff7fd54f96cd
prith <73299952+prith25@users.noreply.github.com>
2020-12-01 18:40:50 +0530

    Delete sum_of_digits.py

--- Recursion/python/sum_of_digits.py
+++
@@ -1,9 +0,0 @@
-#Sum of the individual digits
-def sumofdigits(n):
-    assert n > 0 and int(n) == n, "Enter only integer or Enter only the positive numbers"
-    if n < 10:
-        return n
-    else:
-        return n%10 + sumofdigits(n//10)
-
-print(sumofdigits(123456789))
